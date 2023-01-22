8b28b2dc878701dbb41fe38e8c435cb22b43c12a
MrMindy <gnunes1530@gmail.com>
2023-01-22 14:16:50 -0300

    Setting up app layout, Adding all card assets and setting button corner radius

---
+++ CardStop/CardViewController.swift
@@ -0,0 +1,49 @@
+//
+//  CardViewController.swift
+//  CardStop
+//
+//  Created by Gustavo Nunes Pereira on 22/01/23.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    //An Outlet allow access into functions and data from it variable
+    @IBOutlet var cardImage: UIImageView!
+    
+    @IBOutlet var stopButton: UIButton!
+    @IBOutlet var restartButton: UIButton!
+    @IBOutlet var rulesButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        setButtonsRadius(
+            buttons: [
+                stopButton,
+                restartButton,
+                rulesButton
+            ]
+        );
+        
+    }
+    
+    //An action means that something will happen when (In this example) the user tap the button
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
+    
+    func setButtonsRadius(buttons: [UIButton], radius: CGFloat = 8) {
+        
+        for button in buttons {
+            button.layer.cornerRadius = radius;
+        }
+        
+    }
+}
